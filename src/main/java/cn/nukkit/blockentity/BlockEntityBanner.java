706bd253bffa4b1d17541c4c778268140f442cc8
3317 <gekai@live.jp>
2017-12-11 23:16:58 +0900

    Base

---
+++ src/main/java/cn/nukkit/blockentity/BlockEntityBanner.java
@@ -0,0 +1,18 @@
+package cn.nukkit.blockentity;
+
+import cn.nukkit.Player;
+import cn.nukkit.item.Item;
+import cn.nukkit.block.Block;
+import cn.nukkit.level.format.FullChunk;
+import cn.nukkit.nbt.tag.CompoundTag;
+import cn.nukkit.utils.DyeColor;
+
+public class BlockEntityBed extends BlockEntitySpawnable {
+
+    public int color;
+	
+	
+	public DyeColor getDyeColor() {
+		return DyeColor.getByWoolData(color);
+	}
+}
