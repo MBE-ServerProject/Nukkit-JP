eec06be92b34c16546f7432556c68566ccd54ebe
Meshida <meshida.200202@gmail.com>
2017-12-13 01:47:14 +0900

    color update

--- src/main/java/cn/nukkit/block/BlockWallBanner.java
+++ src/main/java/cn/nukkit/block/BlockWallBanner.java
@@ -66,7 +66,7 @@ public class BlockWallBanner extends BlockTransparent {
                     .putInt("x", (int) block.x)
                     .putInt("y", (int) block.y)
                     .putInt("z", (int) block.z)
-                    .putByte("color", item.getDamage());
+                    .putByte("color", getColor());
 
             if (face == BlockFace.UP) {
                 meta = (int) Math.floor(((player.yaw + 180) * 16 / 360) + 0.5) & 0x0f;
